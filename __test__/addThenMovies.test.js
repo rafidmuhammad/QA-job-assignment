2ebc009f0b1591f8b9792fa7d4f458483c2ac8a6
Rafid Muhammad <rafid.muhammad@gmail.com>
2023-01-28 21:48:04 +0700

    create add and all movies integration

--- __test__/addThenMovies.test.js
+++ __test__/addThenMovies.test.js
@@ -18,25 +18,24 @@ describe("Integration for add a movie and get all movies", () => {
           runtime : "120", 
           gross : "Rp.1M"
     }
+    const expected = {title: "Life of Rafid", description: "Film tentang filantropi"}
     test("First add a movie", async () => {
         const title = "Life of Rafid"
- 
         response  = await axios.post(`http://localhost:5000/add-movie`,body);
         expect(response.status).toBe(201);
         expect(response.data).toHaveProperty('status','Created');
         expect(response.data).toHaveProperty('message','Data added successfully');
         expect(response.data).toHaveProperty('data');
         expect(response.data.data).toHaveProperty('title',title);
-
     })
 
     test("Then get the movies and validate if the object is contained", async() => {
        try {
-	    response = await axios.get(`http://localhost:5000/allmovie/`);
+	    response = await axios.get(`http://localhost:5000/allmovie`);
         expect(response.status).toBe(200);
         expect(response.data).toHaveProperty('statusCode',200);
         expect(response.data).toHaveProperty('status','success');
-        expect(response.data.data).toEqual(expect.objectContaining(body));
+        expect(response.data.data.movies).toContainEqual(expected);
 	        
         } catch (error) {
             expect(error.response.status).toBe(404);
