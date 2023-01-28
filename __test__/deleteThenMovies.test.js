c959d43ce52f4137bf3b46555eb9be4f56754273
Rafid Muhammad <rafid.muhammad@gmail.com>
2023-01-28 22:15:44 +0700

    create delete and get movies integration

---
+++ __test__/deleteThenMovies.test.js
@@ -0,0 +1,53 @@
+const {axios, matchersWithOptions } = require("../testDepend");
+
+expect.extend(matchersWithOptions({
+    verbose: true
+}))
+
+
+describe("Integration for delete and get movies should not contain the data added priorly", () => {
+    let response;
+
+    const expectedBody = {title : "Life of Rafid", description : "Film tentang filantropi"}
+
+    beforeAll(async () => {
+        const title = "Life of Rafid"
+        const body = {
+            movies : `${title}`,
+            year : "2022",
+            genre : "Hidup", 
+            rating : "10",
+             one_line : "Film tentang filantropi",
+              stars : "Rafid Muhammad", 
+              votes : "1000000", 
+              runtime : "120", 
+              gross : "Rp.1M"
+        }
+        response  = await axios.post(`http://localhost:5000/add-movie`,body);
+        expect(response.status).toBe(201);
+        expect(response.data.data).toHaveProperty('title',title);
+    })
+
+    test("First we delete the data", async() => {
+        const title = "Life_of_Rafid"
+        response = await axios.delete(`http://localhost:5000/delete-movie/${title}`);
+        expect(response.status).toBe(204);
+    })
+
+    test("Then get the movies and validate if the added object priorly is not contained", async() => {
+        try {
+         response = await axios.get(`http://localhost:5000/allmovie`);
+         expect(response.status).toBe(200);
+         expect(response.data).toHaveProperty('statusCode',200);
+         expect(response.data).toHaveProperty('status','success');
+         expect(response.data.data.movies).not.toContainEqual(expectedBody);
+             
+         } catch (error) {
+             expect(error.response.status).toBe(404);
+             expect(error.response.data).toHaveProperty('statusCode',404);
+             expect(error.response.data).toHaveProperty('status','Not Found');
+             expect(error.response.data).toHaveProperty('message', "Not Found");
+         }
+      })
+
+})
